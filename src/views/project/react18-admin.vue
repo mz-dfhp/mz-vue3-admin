8b40dcb200320590ba951aac06f8644fdc90178c
lc <486836332@qq.com>
2023-06-14 17:52:01 +0800

    chore: other

--- src/views/project/react18-admin.vue
+++ src/views/project/react18-admin.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="">
-    <el-button type="primary">react18-admin</el-button>
+    <el-button type="primary" @click="onGoToUrl">react18-admin</el-button>
   </div>
 </template>
 
-<script setup lang="ts" name="React18-admin"></script>
+<script setup lang="ts" name="React18-admin">
+const onGoToUrl = () => {
+  window.open('https://github.com/mz-dfhp/mz-design')
+}
+</script>
 
 <style scoped lang="scss"></style>
